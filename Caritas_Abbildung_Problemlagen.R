b55efde427396af6173cce5afc581012e76178d3
sve-n <92168158+sve-n@users.noreply.github.com>
2021-11-20 22:08:16 +0100

    create R file for plot "Problemlagen"

---
+++ Caritas_Abbildung_Problemlagen.R
@@ -0,0 +1,127 @@
+library(tidyverse)
+library(dplyr)
+source("C:/Users/Caritas_Data_Cleaning_2010_to_2021.R")
+
+# Recoding
+
+problems2_2010 <- problems2_2010 %>% 
+  mutate(Problemlage = case_when(
+    Problemlage %in% c("Krankheit", "psychische Probleme") ~ "Krankheit (auch psychische Probleme)",
+    TRUE ~ Problemlage)) %>%
+  group_by(Problemlage) %>%
+  summarize_all(sum)
+
+problems2_2011 <- problems2_2011 %>% 
+  mutate(Problemlage = case_when(
+    Problemlage %in% c("Krankheit", "psychische Probleme") ~ "Krankheit (auch psychische Probleme)",
+    TRUE ~ Problemlage)) %>%
+  group_by(Problemlage) %>%
+  summarize_all(sum)
+
+problems2_2012 <- problems2_2012 %>% 
+  mutate(Problemlage = case_when(
+    Problemlage %in% c("Krankheit", "psychische Probleme") ~ "Krankheit (auch psychische Probleme)",
+    TRUE ~ Problemlage)) %>%
+  group_by(Problemlage) %>%
+  summarize_all(sum)
+
+problems2_2013 <- problems2_2013 %>% 
+  mutate(Problemlage = case_when(
+    Problemlage %in% c("Krankheit", "psychische Probleme") ~ "Krankheit (auch psychische Probleme)",
+    TRUE ~ Problemlage)) %>%
+  group_by(Problemlage) %>%
+  summarize_all(sum)
+
+problems2_2014 <- problems2_2014 %>% 
+  mutate(Problemlage = case_when(
+    Problemlage %in% c("Krankheit", "psychische Probleme") ~ "Krankheit (auch psychische Probleme)",
+    TRUE ~ Problemlage)) %>%
+  group_by(Problemlage) %>%
+  summarize_all(sum)
+
+problems2_2015 <- problems2_2015 %>% 
+  mutate(Problemlage = case_when(
+    Problemlage %in% c("Krankheit", "psychische Probleme") ~ "Krankheit (auch psychische Probleme)",
+    TRUE ~ Problemlage)) %>%
+  group_by(Problemlage) %>%
+  summarize_all(sum)
+
+problems2_2016 <- problems2_2016 %>% 
+  mutate(Problemlage = case_when(
+    Problemlage %in% c("Energieschulden", "Mietschulden") ~ "Energie- oder Mietschulden",
+    Problemlage %in% c("Krankheit", "psychische Probleme") ~ "Krankheit (auch psychische Probleme)",
+    TRUE ~ Problemlage)) %>%
+  group_by(Problemlage) %>%
+  summarize_all(sum)
+
+problems2_2017 <- problems2_2017 %>% 
+  mutate(Problemlage = case_when(
+    Problemlage %in% c("Energieschulden", "Mietschulden") ~ "Energie- oder Mietschulden",
+    Problemlage %in% c("Krankheit", "psychische Probleme") ~ "Krankheit (auch psychische Probleme)",
+    TRUE ~ Problemlage)) %>%
+  group_by(Problemlage) %>%
+  summarize_all(sum)
+
+problems2_2018 <- problems2_2018 %>% 
+  mutate(Problemlage = case_when(
+    Problemlage %in% c("Energieschulden", "Mietschulden") ~ "Energie- oder Mietschulden",
+    Problemlage %in% "Schulden allgemein" ~ "Schulden",
+    TRUE ~ Problemlage),
+    ) %>%
+  group_by(Problemlage) %>%
+  summarize_all(sum)
+
+problems2_2019 <- problems2_2019 %>% 
+  mutate(Problemlage = case_when(
+    Problemlage %in% c("Energieschulden", "Mietschulden") ~ "Energie- oder Mietschulden",
+    Problemlage %in% "Schulden allgemein" ~ "Schulden",
+    TRUE ~ Problemlage)) %>%
+  group_by(Problemlage) %>%
+  summarize_all(sum)
+
+problems2_2020 <- problems2_2020 %>% 
+  mutate(Problemlage = case_when(
+    Problemlage %in% c("Energieschulden", "Mietschulden") ~ "Energie- oder Mietschulden",
+    Problemlage %in% "Schulden allgemein" ~ "Schulden",
+    Problemlage %in% "Krankheit" ~ "Krankheit (auch psychische Probleme)",
+    TRUE ~ Problemlage)) %>%
+  group_by(Problemlage) %>%
+  summarize_all(sum)
+
+problems2_2021 <- problems2_2021 %>% 
+  mutate(Problemlage = case_when(
+    Problemlage %in% c("Energieschulden", "Mietschulden") ~ "Energie- oder Mietschulden",
+    Problemlage %in% "Schulden allgemein" ~ "Schulden",
+    Problemlage %in% "Krankheit" ~ "Krankheit (auch psychische Probleme)",
+    TRUE ~ Problemlage)) %>%
+  group_by(Problemlage) %>%
+  summarize_all(sum)
+
+# Zeitreihe
+
+ts_problems <- list(problems2_2010, problems2_2011, problems2_2012, problems2_2013,
+                    problems2_2014, problems2_2015, problems2_2016, problems2_2017,
+                    problems2_2018, problems2_2019, problems2_2020, problems2_2021) %>%
+  reduce(left_join, by = "Problemlage")
+
+ts_problems <- ts_problems %>% select(Problemlage, contains("Prozent"))
+colnames(ts_problems) <- c("Problemlage", "2010", "2011", "2012", "2013", "2014", "2015", "2016",
+                           "2017", "2018", "2019", "2020", "2021")
+
+ts_problems2 <- ts_problems %>% pivot_longer(!Problemlage, 
+                                     names_to = "Jahr", 
+                                     values_to = "Prozent") %>%
+  mutate(Problemlage = factor(Problemlage,
+                              levels = c("Alter/Pflegebedürftigkeit", "Behinderung", 
+                                         "Krankheit (auch psychische Probleme)", "Schwangerschaft", 
+                                         "Arbeitslosigkeit", "Sanktionen nach SGB II", "sozialrechtliche Probleme", 
+                                         "Umgang mit Behörde", "Schulden", "Energie- oder Mietschulden", 
+                                         "Sonstige finanzielle Schwierigkeiten", "Probleme im Bereich Wohnen", 
+                                         "Partner/Erziehung/familiäre Probleme", "Sonstiges")))
+
+plot_problems <- ts_problems2 %>%
+  filter(Problemlage != "Sonstiges") %>%
+  ggplot(aes(x = Jahr, y = Prozent, group = Problemlage, color = Problemlage)) +
+  geom_line(size = 1.1) + 
+  theme_linedraw() 
+ggsave(plot_problems, file = "plot_problems.jpeg", unit = "cm", height = 12, width = 21, dpi = 500)
